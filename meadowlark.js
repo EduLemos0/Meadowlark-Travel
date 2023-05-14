aaddb25f73dd301717985212485138132491be64
Eduardo Lemos <dudulemox@gmail.com>
2023-05-13 22:41:02 -0300

    Changed JS syntax.

--- meadowlark.js
+++ meadowlark.js
@@ -1,43 +1,29 @@
-import express from "express";
-import { engine } from "express-handlebars";
-import { getFortune } from "./lib/fortune.js";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+const express = require("express");
+const handlebars = require("express-handlebars");
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const handlers = require("./lib/handlers");
 
 const app = express();
 
-/** Set up Handlebar engine */
-app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    defaultLayout: "main",
+  })
+);
 app.set("view engine", "handlebars");
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
-const port = process.env.PORT || 8080;
+app.get("/", handlers.home);
 
-/** Routes */
+app.get("/about", handlers.about);
 
-app.get("/", (req, res) => res.render("home"));
-
-app.get("/about", (req, res) => {
-  res.render("about", { fortune: getFortune() });
-});
-
-/** Middlewares */
-
-// customized 404 page
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
-
-// customized 500 page
-app.use((req, res) => {
-  res.status(500);
-  res.render("500");
-});
+app.use(handlers.notFound);
+app.use(handlers.serverError);
 
 app.listen(port, () =>
-  console.log(`Express running on port ${port}; Press Ctrl-C to terminate.....`)
+  console.log(`Express running on port ${port}; Press Ctrl+C to terminate...`)
 );
